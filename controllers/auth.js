e656b7d78c6ba2d3617d7999fafe2bcdc52a523f
Maria896 <mariasaleem896@gmail.com>
2023-08-26 12:09:37 +0500

    Registratio with email verification done

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,8 @@ import User from "../schemas/userSchema.js";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
 import nodemailer from "nodemailer";
-import Workspace from "../schemas/workspaceSchema.js"
-import JWT from 'jsonwebtoken';
-
+import Workspace from "../schemas/workspaceSchema.js";
+import JWT from "jsonwebtoken";
 
 // Path     :   /api/auth/signup
 // Method   :   Post
@@ -17,38 +16,52 @@ export const signup = async (req, res) => {
 		const existedUser = await User.find({ email: email });
 		console.log(existedUser);
 		if (!existedUser) {
-			return res.status(400).json({
-				success: false,
-				error: true,
-				message: "User already exists please try another email",
+			if (existedUser.isVerified) {
+				return res.status(400).json({
+					success: false,
+					error: true,
+					message: "User already exists please try another email",
+				});
+			} else {
+				await sendEmailVerificationLink(
+					existedUser.email,
+					existedUser.verificationToken,
+					existedUser._id
+				);
+
+				res.status(200).json({
+					success: true,
+					error: false,
+					message:
+						"Signup successful. Please check your email to verify your account.",
+					user,
+				});
+			}
+		} else {
+			const verificationToken = generateVerificationToken();
+			const hashPassword = hashedPassword(password);
+
+			const user = await new User({
+				fullName,
+				email,
+				password: hashPassword,
+				verificationToken: verificationToken,
+				isVerified: false,
+			}).save();
+			await sendEmailVerificationLink(
+				user.email,
+				user.verificationToken,
+				user._id
+			);
+
+			res.status(200).json({
+				success: true,
+				error: false,
+				message:
+					"Signup successful. Please check your email to verify your account.",
+				user,
 			});
 		}
-		const verificationToken = generateVerificationToken();
-		const hashPassword = hashedPassword(password);
-
-		const user = await new User({
-			fullName,
-			email,
-			password: hashPassword,
-			verificationToken: verificationToken,
-			isVerified: false,
-		}).save();
-		await sendEmailVerificationLink(
-			user.email,
-			user.verificationToken,
-			user._id
-		);
-		const workspace = await new Workspace({
-			name: `${user.fullName}Workspace`,
-			user: user._id
-		}).save()
-		res.status(200).json({
-			success: true,
-			error: false,
-			message:
-				"Signup successful. Please check your email to verify your account.",
-			user,
-		});
 	} catch (err) {
 		console.log(err);
 		res
@@ -61,57 +74,85 @@ export const signup = async (req, res) => {
 // Access   :   Private
 // Desc     :   Verifiy email of new user
 export const verifyEmail = async (req, res) => {
-    try {
-		const id = req.params.id
-		console.log(req.params.id)
-        const user = await User.find({ id: id });
-		console.log(user)
-        if (!user) return res.status(400).json("Invalid link");
-		const verifiedUser = await User.findByIdAndUpdate(id, {isVerified: true}, {
-			new: true, 
-		  });
-       
-        res.status(200).json({ message: "Your email has been verified successfully",verifiedUser });
-    } catch (error) {
-        res.status(400).json({ message: "error", error });
-    }
-}
+	try {
+		const id = req.params.id;
+		console.log(req.params.id);
+		const user = await User.find({ id: id });
+		console.log(user);
+		if (!user) return res.status(400).json("Invalid link");
+		const verifiedUser = await User.findByIdAndUpdate(
+			id,
+			{ isVerified: true },
+			{
+				new: true,
+			}
+		);
+		const workspace = await new Workspace({
+			name: `${user.fullName}Workspace`,
+			user: user._id,
+		}).save();
+		res.status(200).json({
+			message: "Your email has been verified successfully",
+			verifiedUser,
+		});
+	} catch (error) {
+		res.status(400).json({ message: "error", error });
+	}
+};
 
 // Path     :   /api/auth/signin
 // Method   :   Post
 // Access   :   Private
 // Desc     :   Login User
 export const signin = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
-    try {
-        if (!email || !password) {
-            return res.status(400).json({ success: false, error: true, message: "Email and Password is required" })
-        }
-        if (!user) {
-            return res.status(400).json({ success: false, error: true, message: "Email is not registered" })
-        }
-        const compare = bcrypt.compareSync(password, user.password);
-        if (!compare) {
-            return res.status(400).json({ success: false, error: true, message: "Invalid password please enter valid password" })
-        }
-        if (!user.isVerified) {
-            return res.status(400).json({ success: false, error: true, message: "Please verify your email" })
-        }
-		const sceretKey = process.env.JWT_SECRET
+	const { email, password } = req.body;
+	const user = await User.findOne({ email: email });
+	try {
+		if (!email || !password) {
+			return res.status(400).json({
+				success: false,
+				error: true,
+				message: "Email and Password is required",
+			});
+		}
+		if (!user) {
+			return res.status(400).json({
+				success: false,
+				error: true,
+				message: "Email is not registered",
+			});
+		}
+		const compare = bcrypt.compareSync(password, user.password);
+		if (!compare) {
+			return res.status(400).json({
+				success: false,
+				error: true,
+				message: "Invalid password please enter valid password",
+			});
+		}
+		if (!user.isVerified) {
+			return res.status(400).json({
+				success: false,
+				error: true,
+				message: "Please verify your email",
+			});
+		}
+		const sceretKey = process.env.JWT_SECRET;
 
-        const token = JWT.sign({ _id: user._id },sceretKey,{ expiresIn: '7d' });
-        res.status(200).json({
-            success: true,
-            error: false,
-            message: "Login successfully",
-            token: token,
-            user: user,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({ success: false, error: true, message: "network error" })
-    }
+		const token = JWT.sign({ _id: user._id }, sceretKey, { expiresIn: "7d" });
+		res.status(200).json({
+			success: true,
+			error: false,
+			message: "Login successfully",
+			token: token,
+			user: user,
+		});
+	} catch (error) {
+		console.log(error);
+		res
+			.status(400)
+			.json({ success: false, error: true, message: "network error" });
+	}
 };
 // Function for creating verification token
 const generateVerificationToken = () => {
@@ -130,12 +171,12 @@ const sendEmailVerificationLink = async (to, token, id) => {
 	console.log(to, token);
 	try {
 		const transporter = nodemailer.createTransport({
-			host: 'smtp.ethereal.email',
+			host: "smtp.ethereal.email",
 			port: 587,
 			auth: {
-				user: 'arielle.schinner@ethereal.email',
-				pass: 'WpjTg4nvAabZmeKjuy'
-			}
+				user: "arielle.schinner@ethereal.email",
+				pass: "WpjTg4nvAabZmeKjuy",
+			},
 		});
 		await transporter.sendMail({
 			from: "admin@gmail.com",
