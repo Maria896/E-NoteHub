e568a111e4b4e92db1032c8756e75f675780adcb
Maria Saleem <mariasaleem896@gmail.com>
2023-08-29 18:19:44 +0500

    Adding swagger documentations of APIs

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ import userRoutes from "./routes/user.js";
 import workspaceRoutes from "./routes/workspace.js";
 import otpRoutes from "./routes/otp.js";
 import { authHandler } from "./middlewares/auth.js";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs"
 
 const app = express();
+const swaggerJSDocs = YAML.load("./api.yaml");
 dotenv.config();
 ConnectDB();
 
@@ -19,11 +22,12 @@ ConnectDB();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSDocs));
 
 app.use("/api/tags",authHandler, tagRoutes);
 app.use("/api/notes",authHandler, notesRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/user",authHandler,  userRoutes);
+app.use("/api/user",  userRoutes);
 app.use("/api/workspace", authHandler, workspaceRoutes);
 app.use("/api/otp", otpRoutes);
 
